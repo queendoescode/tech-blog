3e826a297233a2055983899ee6b40e5312c0441f
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-09-02 16:56:40 -0400

    added page for edit post and frontend javascript form submit handlers.

--- public/js/post.js
+++ public/js/post.js
@@ -20,8 +20,58 @@ const postFormHandler = async (event) => {
   }
 };
 
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  const postId = document.querySelector('#post-id').value;
+
+  if (title && body) {
+    const response = await fetch(`/api/post/${postId}`, {
+      method: 'PUT',
+      body: JSON.stringify( { title, body } ),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/dashboard`);
+    } else {
+      alert('There was a problem updating the post.');
+    }
+  }
+};
+
+const deleteFormHandler = async (event) => {
+  event.preventDefault();
+
+  const postId = document.querySelector('#post-id').value;
+
+  const response = await fetch(`/api/post/${postId}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard`);
+  } else {
+    alert('There was a problem deleting the post.');
+  }
+};
+
 const postForm = document.querySelector('#post-form');
 
 if (postForm) {
   postForm.addEventListener('submit', postFormHandler);
+}
+
+const updatePostForm = document.querySelector('#update-post-form');
+
+if (updatePostForm) {
+  updatePostForm.addEventListener('submit', updateFormHandler);
+}
+
+const deletePostForm = document.querySelector('#delete-post-form');
+
+if (deletePostForm) {
+  deletePostForm.addEventListener('submit', deleteFormHandler);
 }
